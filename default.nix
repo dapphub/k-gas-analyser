72dc52b8d08386c831190ff19226b184bfdd0e62
Lev Livnev <lev@liv.nev.org.uk>
2018-11-26 18:46:41 +0000

    default.nix: try approach of locking a specific commit

--- default.nix
+++ default.nix
@@ -1,2 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./analyser.nix { }
+{
+  nixpkgs ? (import <nixpkgs> {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "2c2ab68672a34e7c226ff0c5ecf4d2e063ac5705";
+    sha256 = "00rsmyhq50c44ayiwcaz8ck2043lrywyj9mj4cad8aw8f3l6yq6x";
+  },
+  compiler ? "ghc822"
+}:
+let
+  pkgs = import nixpkgs { config = {}; };
+in
+  pkgs.haskell.packages.${compiler}.callPackage ./analyser.nix { }
